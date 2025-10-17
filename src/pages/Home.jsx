93ac0bb55caf999f68c2d093db814bef5c0cfe65
sebaaburto <seba.aburto@duocuc.cl>
2025-10-17 00:41:30 -0300

    primera parte de Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,50 @@
 import React from 'react'
 import HeaderComponents from '../components/Header/HeaderComponents'
 import FooterComponents from '../components/Footer/FooterComponent' 
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
+import styles from "../styles/home.module.css"
+import placeholderImg from "../assets/nosotros/placeholder.png"
 
 function HomePages() {
   return (
     <>
-        <HeaderComponents/>
+      <HeaderComponents/>
 
-        <div class="user-links">
-          <Link to="/login">Iniciar sesión</Link> |
-           <Link to="/register"> Registrarse</Link>
-        </div>
+      
+      <div className={styles.userLinks}>
+        <Link to="/login">Iniciar sesión</Link> |
+        <Link to="/register"> Registrarse</Link>
+      </div>
 
-        <div>Pagina principal</div>
+     
+      <div className={styles.heroImage}>
+        <img src={placeholderImg} alt="Imagen destacada" />
+      </div>
 
-        <FooterComponents/>
+      
+      <div className={styles.heroText}>
+        <h1>¡Celebramos 50 años!</h1>
+        <p>
+          Pastelería Mil Sabores celebra sus 50 años endulzando la vida de los chilenos. Desde 1975, 
+          hemos sido parte de tus momentos más especiales, consolidándonos como un referente de la 
+          repostería de calidad. Nuestra historia, marcada por hitos como el récord Guinness de 1995, 
+          es un testimonio de nuestra dedicación.
+          <br /><br />
+          Para celebrar este importante aniversario, hemos renovado nuestra tienda online. 
+          Ahora puedes disfrutar de nuestros productos de alta calidad de forma más sencilla y rápida, 
+          desde la comodidad de tu hogar. 
+          <br /><br />
+          Usa el código <b>FELICES50</b> y obtén un descuento del 10% en tu próxima compra, mientras disfrutas 
+          de la calidad y el sabor que solo nosotros podemos ofrecer.
+        </p>
+
+       
+        <Link to="/productos">
+          <button className={styles.btnProductos}>Ver productos</button>
+        </Link>
+      </div>
+
+      <FooterComponents/>
     </>
   )
 }
